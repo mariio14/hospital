3edfb5d6db050be07cdc836d15b05f0e86e38e2c
mario <m.folgarf@udc.es>
2025-04-21 19:43:08 +0200

    corrección decode

--- myTfg/src/main/java/es/udc/fi/tfg/model/services/clingo/decode_monthly.py
+++ myTfg/src/main/java/es/udc/fi/tfg/model/services/clingo/decode_monthly.py
@@ -8,27 +8,35 @@ if len(sys.argv) < 2:
     sys.exit()
 
 ctl = clingo.Control()
+
+ctl.configuration.solve.opt_mode = "opt"
+
 for arg in sys.argv[1:]:
     ctl.load(arg)
 ctl.ground([("base", [])])
 
 with ctl.solve(yield_=True) as handle:
-    assignments = {}
-
+    optimal_model = None
     for model in handle:
-        for atom in model.symbols(atoms=True):
-            if atom.name == "day_assign" and len(atom.arguments) == 3:
-                person = str(atom.arguments[0])
-                day = atom.arguments[1].number
-                activity = str(atom.arguments[2])
+        optimal_model = model
 
-                if day in (0, -1):
-                    continue
+assignments = {}
 
-                if person not in assignments:
-                    assignments[person] = {}
-                assignments[person][day] = activity
+if optimal_model:
+    for atom in optimal_model.symbols(shown=True):
+        if atom.name == "day_assign" and len(atom.arguments) == 3:
+            person = str(atom.arguments[0])
+            day = atom.arguments[1].number
+            activity = str(atom.arguments[2])
 
+            if day in (0, -1):
+                continue
+
+            if person not in assignments:
+                assignments[person] = {}
+            assignments[person][day] = activity
+
+# Ordenar para salida bonita
 ordered_assignments = OrderedDict(
     sorted(
         {person: OrderedDict(sorted(days.items())) for person, days in assignments.items()}.items()
